028f93a0b95eb1cdfb30a8e11b6621d3aece709e
Chadwick Boulay <chadwick.boulay@gmail.com>
2013-04-22 18:24:33 +0900

    update submodule ref

---
+++ test_ogre.bat
@@ -0,0 +1,11 @@
+#! ../BCI2000/prog/BCI2000Shell
+@cls & ..\BCI2000\prog\BCI2000Shell %0 %* #! && exit /b 0 || exit /b 1\n
+Change directory $BCI2000LAUNCHDIR
+Show window; Set title ${Extract file base $0}
+Reset system
+Startup system localhost
+Start executable SignalGenerator --local --FileFormat=Null
+Start executable SpectralSignalProcessing --local
+Start executable PythonApplication --local --PythonAppClassFile=..\..\BCPyElectrophys\OgreApplication.py --PythonAppWD=..\..\BCPyElectrophys
+Wait for Connected
+Load parameterfile "../../BCPyElectrophys/test.prm"
